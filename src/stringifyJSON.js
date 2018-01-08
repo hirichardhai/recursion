982a5b51fd7eee8c7c235af969da650561140698
Richard Hai <hirichardhai@gmail.com>
2018-01-08 13:26:38 -0800

    Added string case for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,7 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+	if (typeof obj === 'string') {
+		return '"' + obj + '"';
+	}
+}
